d5e70b23583156375972d29d7b87df34c79f1146
NIck1821 <personallune@mail.ru>
2020-10-26 00:38:50 +0400

    Commit.2

--- src/test/java/com/automationPractice/VoidEmail.java
+++ src/test/java/com/automationPractice/VoidEmail.java
@@ -2,25 +2,17 @@ package com.automationPractice;
 
 import com.automationPractice.config.WebDriverSettings;
 import junitparams.JUnitParamsRunner;
-import junitparams.Parameters;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(JUnitParamsRunner.class)
-public class EmailCorrectTest extends WebDriverSettings {
+public class VoidEmail extends WebDriverSettings {
 
     @Test
-    @Parameters({
-            "Inalune@mail.ru",
-            "324423432@mail.ru",
-            "Ina.lune@mail.ru",
-    })
-    public void CorrectTest(String email) {
+    public void VoidEmailTest() {
         String title = chromeDriver.getTitle();
         Assert.assertTrue(title.equals("Login - My Store"));
-        regPage.setEmailAddress(email);
         regPage.clickCreateAnAccount();
     }
-
 }
--- src/test/java/com/automationPractice/config/WebDriverSettings.java
+++ src/test/java/com/automationPractice/config/WebDriverSettings.java
@@ -1,9 +1,8 @@
 package com.automationPractice.config;
 
+import com.automationPractice.Page.FullRegPage;
 import com.automationPractice.Page.RegPage;
-import org.junit.After;
 import org.junit.AfterClass;
-import org.junit.Before;
 
 import org.junit.BeforeClass;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -15,12 +14,14 @@ public class WebDriverSettings {
     public static ChromeDriver chromeDriver;
     public static String PATH_CHROME_DRIVER = ConfigProperties.getProperty("chromedriver");
     public static RegPage regPage;
+    public static FullRegPage fullRegPage;
 
     @BeforeClass
     public static void setUp() {
         System.setProperty("webdriver.chrome.driver", PATH_CHROME_DRIVER);
         chromeDriver = new ChromeDriver();
         regPage = new RegPage(chromeDriver);
+        fullRegPage = new FullRegPage(chromeDriver);
 
         chromeDriver.manage().window().maximize();
         chromeDriver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
