81eb0097ae6013f1fbbadf1e2e55b021e4c6a8e8
NIck1821 <personallune@mail.ru>
2020-10-28 00:20:30 +0400

    Commit.6

--- src/test/java/com/automationPractice/FullCorrectRegPage.java
+++ src/test/java/com/automationPractice/FullCorrectRegPage.java
@@ -4,14 +4,12 @@ import com.automationPractice.config.ConfigProperties;
 import com.automationPractice.config.WebDriverSettings;
 import junitparams.FileParameters;
 import junitparams.JUnitParamsRunner;
-import junitparams.Parameters;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
-import java.util.List;
 
 @RunWith(JUnitParamsRunner.class)
 public class FullCorrectRegPage extends WebDriverSettings {
@@ -23,7 +21,7 @@ public class FullCorrectRegPage extends WebDriverSettings {
         chromeDriver.get(ConfigProperties.getProperty("registrationPage"));
         chromeDriver.findElement(By.id("email_create")).sendKeys("Inalune@mail.ru");
         chromeDriver.findElement(By.id("SubmitCreate")).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("account-creation_form")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("center_column")));
 
         fullRegPage.setFirstName(firstName);
         fullRegPage.setLastName(lastName);
@@ -31,6 +29,6 @@ public class FullCorrectRegPage extends WebDriverSettings {
 
         fullRegPage.clickRegister();
 
-//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("alert alert-danger")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("center_column")));
     }
 }
